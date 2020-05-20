f4aa7184e93abb0713606dad2da54a5c697000ca
zerbinidamata <rafinhazerbini@gmail.com>
2020-05-20 13:11:42 -0300

    first commit

---
+++ api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/rs/cors"
+
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	// Connect to mongo
+	session, err := mgo.Dial("mongo:27017")
+	if err != nil {
+		log.Fatalln(err)
+		log.Fatalln("mongo err")
+		os.Exit(1)
+	}
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	// Get posts collection
+	posts = session.DB("app").C("posts")
+
+	// Set up routes
+
+
+	http.ListenAndServe(":8080", cors.AllowAll().Handler(r))
+	log.Println("Listening on port 8080...")
+}
+
+
---
+++ api/sanity_test.go
@@ -0,0 +1,8 @@
+  
+package main
+
+import "testing"
+
+func TestSanity(t *testing.T) {
+	t.Log("Has sanity")
+}
